12006efea09ecd9870406ca72d67a797f11a2c8b
Mark Kudlac <mark.kudlac@sympatico.ca>
2015-02-19 21:37:20 -0500

    HTML and images being merged properly

--- app/webapp/SysHtml/TestApp/localjs/client.js
+++ app/webapp/SysHtml/TestApp/localjs/client.js
@@ -1,2 +1,16 @@
 
-alert("This is client.js")
+
+function initbut() {
+
+console.log("In initbut client.js");
+
+ $("#lastnamebut").on("click", alertbut)
+
+}
+
+
+function alertbut(){
+
+    alert("This is client.js")
+    processimg($("#pic1"), "/img/mark_head2.jpg")
+}
